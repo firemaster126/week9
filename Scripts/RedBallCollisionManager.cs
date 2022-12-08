78786e16f0ec9dd2591fdd09fbcb8b6e45e454fe
firemaster126 <114187919+firemaster126@users.noreply.github.com>
2022-12-07 22:53:59 -0500

    Add files via upload

---
+++ Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public AudioSource vineboomsound;
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        vineboomsound.Play();
+    }
+}
+
